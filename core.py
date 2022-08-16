3d9931b6d29b722c5b79f5b1606a4fdb1c0d482f
thiagorbm <thiagorbm@gmail.com>
2022-08-16 14:26:54 -0300

    second commit

--- core.py
+++ core.py
@@ -15,18 +15,19 @@ def mover(initial, final):
         print("ouvindo")
         newfiles = os.listdir(initial)
         for file in newfiles:
-            source = initial+"/"+file
-            fileData= os.path.getctime(source)
-            #print(fileData)
-            if fileData >= settings.TIMESTAMP:
-                n = 1
-                target = newname
-                while os.path.exists(target):
-                    n = n+1 
-                    target = f"{newname}_{n}_{extension}"
-                shutil.move(source, target)
-                print(f"arquivo {target} movido")
-            time.sleep(1)
+            if extension in file:
+                source = initial+"/"+file
+                fileData= os.path.getctime(source)
+                #print(fileData)
+                if fileData >= settings.TIMESTAMP:
+                    n = 1
+                    target = newname
+                    while os.path.exists(target):
+                        n = n+1 
+                        target = f"{newname}_{n}_{extension}"
+                    shutil.move(source, target)
+                    print(f"arquivo {target} movido")
+                time.sleep(1)
 
 #     print(settings.DATA.strftime('%Y_%m_%d'))
 #     print(initial)
