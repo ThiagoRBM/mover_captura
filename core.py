eae1837eb7f9e3fdd62b4c525631a7cc67a7cd9b
thiagorbm <thiagorbm@gmail.com>
2022-08-18 08:43:08 -0300

    commit

--- core.py
+++ core.py
@@ -10,9 +10,12 @@ def mover(initial, final):
     name = f"captura_{settings.DATA.strftime('%Y_%m_%d_%H%M%S')}"
     extension = ".png"
     newname = final+"/" + name 
+    n=0
 
     while True:
-        print("ouvindo")
+        n=n+1
+        if n%500000==0:
+            print("ouvindo")
         newfiles = os.listdir(initial)
         for file in newfiles:
             if extension in file:
@@ -20,10 +23,8 @@ def mover(initial, final):
                 fileData= os.path.getctime(source)
                 #print(fileData)
                 if fileData >= settings.TIMESTAMP:
-                    n = 1
                     target = newname
                     while os.path.exists(target):
-                        n = n+1 
                         target = f"{newname}_{n}_{extension}"
                     shutil.move(source, target)
                     print(f"arquivo {target} movido")
