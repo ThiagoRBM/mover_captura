c772871ac8b9f6027e512cbeaaf58f04ee560702
thiagorbm <thiagorbm@gmail.com>
2022-08-22 07:38:57 -0300

    commit

--- core.py
+++ core.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
 import os
 import time
+from datetime import date,datetime
 import shutil
 
 import settings
 
 
 def mover(initial, final):
-    name = f"captura_{settings.DATA.strftime('%Y_%m_%d_%H%M%S')}"
+    #name = f"captura_{settings.DATA.strftime('%Y_%m_%d_%H%M%S')}"
     extension = ".png"
-    newname = final + "/" + name 
+    #newname = final + "/" + name 
     n=0
 
     while True:
-        n=n+1
-        if n%500000==0:
-            print("ouvindo")
         newfiles = os.listdir(initial)
         for file in newfiles:
             if extension in file:
                 source = initial+"/"+file
                 fileData= os.path.getctime(source)
-                #print(fileData)
+                #print(file, fileData)
                 if fileData >= settings.TIMESTAMP:
+                    n=n+1
+                    newname= (f"captura_"
+                            f"{datetime.fromtimestamp(fileData).strftime('%Y_%m_%d_%H%M%S')}_")
+                    print("novo",newname)
                     target = final
                     while os.path.exists(target):
                         target = f"{newname}_{n}_{extension}"
                     shutil.move(source, target)
                     print(f"arquivo {target} movido")
-                time.sleep(1)
+                print("ouvindo")
+                time.sleep(2)
